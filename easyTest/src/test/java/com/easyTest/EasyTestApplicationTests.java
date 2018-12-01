ba55023098bb3d67849a4e95f93f44e5d4d45a56
owner <owner@DESKTOP-GSL95SS>
2018-12-01 19:24:52 +0900

    first commit

---
+++ easyTest/src/test/java/com/easyTest/EasyTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.easyTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EasyTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
